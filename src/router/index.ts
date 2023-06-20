4167c6a06f134e843de965cebc16112ef289dcb5
DinushanVimukthi <stdinushan@gmail.com>
2023-06-20 14:16:34 +0530

    Configured shims-vue.d.ts edited index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
-// @ts-ignore
 import Home from '../pages/Home.vue'
+import Login from '../pages/Login.vue';
 
 // @ts-ignore
 const routes = [
@@ -8,6 +8,11 @@ const routes = [
         path: '/',
         name: 'Home',
         component: () => Home
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => Login
     }
 ]
 
