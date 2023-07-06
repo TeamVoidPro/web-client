6fa94fd7b69cd21a80666e354a8505891e4657fd
DinushanVimukthi <stdinushan@gmail.com>
2023-07-06 23:48:04 +0530

    Login

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+// define props
+import {defineProps} from "vue";
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <div class="flex flex-row justify-between items-center bg-gray-800 text-white p-4">
+    <div class="text-4xl text-white">
+      <router-link to="/">Home</router-link>
+    </div>
+    <div class="text-4xl text-white">
+      <router-link to="/about">
+        {{props.title}}
+      </router-link>
+    </div>
+    <div class="text-4xl text-white">
+      <router-link to="/login">Login</router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import Navbar from "../components/Navbar.vue";
+
+
+</script>
+
+<template>
+  <Navbar title="Login Page"/>
+  <div class="">
+    <div class="text-4xl text-black">
+      Login Page
+    </div>  
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
