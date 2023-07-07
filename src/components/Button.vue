388429b8ba68d9ca709375a1dde41d72ed25a11e
viharshaj <84612797+viharshaj@users.noreply.github.com>
2023-07-07 15:08:35 +0530

    Started operator payments page layout

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,4 +1,4 @@
-<template>
+ <template>
     <button class="px-20 py-2 bg-gradient-to-r from-[#253665] to-[#0F72B7] text-white rounded-2xl font-semibold text-xl"><slot></slot></button>
 </template>
 
---
+++ src/pages/operator/OperatorPayments.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="text-9xl text-cyan-700 bg-accent">Hello</div>
+    <Button>OK</Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import Button from "../../components/Button.vue";
+</script>
