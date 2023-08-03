940429c9958842a27e3490ad645a6604f6ef770e
Isurika Arunodi <2020cs015@stu.ucsc.cmb.ac.lk>
2023-08-03 15:52:28 +0530

    Navbar done

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,29 +1,21 @@
-<script setup lang="ts">
-const props = defineProps({
-  title: {
-    type: String,
-    required: true
-  }
-})
-
-</script>
-
 <template>
-  <div class="absolute  top-8 w-full flex flex-row justify-between items-center bg-white text-black p-4">
-    <div class="text-4xl text-white">
-      <router-link to="/">Home</router-link>
+  <div class="relative flex px-48 py-5 justify-between items-center max-2xl:px-36">
+    <img src = "../assets/images/logo-2.png"
+         alt = "Logo"
+         class = "w-[18em] max-2xl:w-[15em]">
+    <div class="absolute inset-x-[33%] flex justify-between items-center w-[34%] max-2xl:w-[38%] h-[4em]">
+      <div class="text-xl font-bold px-4 py-2 rounded-2xl bg-primary text-white">Home</div>
+      <div class="text-xl font-bold px-4 py-2 rounded-2xl">Services</div>
+      <div class="text-xl font-bold px-4 py-2 rounded-2xl">About Us</div>
+      <div class="text-xl font-bold px-4 py-2 rounded-2xl">Contact Us</div>
     </div>
-    <div class="text-4xl text-white">
-      <router-link to="/about">
-        {{props.title}}
-      </router-link>
-    </div>
-    <div class="text-4xl text-white">
-      <router-link to="/login">Login</router-link>
+    <div>
+      <button class="font-bold text-xl px-4 py-2 rounded-2xl text-white bg-secondary">Log in</button>
     </div>
   </div>
 </template>
 
-<style scoped>
+<script setup
+        lang = "ts">
 
-</style>
+</script>
