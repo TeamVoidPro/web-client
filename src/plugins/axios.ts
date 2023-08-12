55966c9b773d7b6af997aa9d0a49bc140785a12c
Danodya Supun <89889362+DaNoDya99@users.noreply.github.com>
2023-08-12 19:35:26 +0530

    Bug Fix

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { employeeStore } from "../store/employeeStore.ts";
 
 const axiosClient = axios.create({
-    baseURL: 'https://localhost:7211/api/',
+    baseURL: 'https://localhost:44397/api/',
 })
 
 axiosClient.interceptors.request.use( async (config) => {
